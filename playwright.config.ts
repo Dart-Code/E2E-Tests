8f4696a7bb954c672e6789ff735f837bffe33d27
Danny Tuppeny <danny@tuppeny.com>
2026-05-21 10:33:20 +0100

    Revert "Try increasing timeouts"
    This reverts commit d79045059b4fa49800e3f6cb239200a092bfe654.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 	workers: 1, // process.env.CI ? 1 : undefined,
 	timeout: 360 * 1000, // 360 sec
 	expect: {
-		timeout: 300 * 1000, // 300 sec
+		timeout: 180 * 1000, // 180 sec
 	},
 	globalTimeout: 30 * 60 * 1000, // 30 min
 	// TODO(dantup): Test this.
@@ -18,8 +18,8 @@ export default defineConfig({
 		['playwright-ctrf-json-reporter', { outputDir: 'test-results', outputFile: 'ctrf-report.json' }]
 	],
 	use: {
-		actionTimeout: 300 * 1000, // 300 sec
-		navigationTimeout: 300 * 1000, // 300 sec
+		actionTimeout: 180 * 1000, // 180 sec
+		navigationTimeout: 180 * 1000, // 180 sec
 		trace: 'retain-on-failure',
 		screenshot: 'on',
 	},
